7e340ee5924ad9011e7470462cb30a9019556a52
ngcanhnam <ngcanhnam123@gmail.com>
2024-01-19 16:27:09 +0700

    first commit

--- obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635928f67f8819db08b0a26db6d2424fa6992fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8173a5cb0a34b2f5416f124efe6e3bffc16bdefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
